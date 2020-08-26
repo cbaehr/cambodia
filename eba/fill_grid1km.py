6cead91b1370384d783b6099054e2633a50dd94e
Christian Baehr <cbaehr@aiddata.wm.edu>
2020-08-26 08:29:54 -0400

    drop deprecated scripts

--- eba/fill_grid1km.py
+++ eba/fill_grid1km.py
@@ -67,6 +67,60 @@ grid_1km_data.drop(["geometry"], axis=1).to_csv(path+"/grid_1km.csv", index=Fals
 
 ###########################################################################
 
+def getValuesAtPoint(indir, rasterfileList, pos, lon, lat, cell_id):
+    for i, rs in enumerate(rasterfileList):
+        presValues = []
+        gdata = gdal.Open('{}/{}'.format(indir,rs))
+        gt = gdata.GetGeoTransform()
+        band = gdata.GetRasterBand(1)
+        nodata = band.GetNoDataValue()
+        x0, y0 , w , h = gt[0], gt[3], gt[1], gt[5]
+        data = band.ReadAsArray().astype(np.float)
+        params = data.shape
+        del gdata
+        if i == 0:
+            for p in pos.iterrows():
+                x = int((p[1][lon] - x0)/w)
+                y = int((p[1][lat] - y0)/h)
+                if y < params[0] and x < params[1]:
+                    val = data[y,x]
+                else:
+                    val = -9999
+                presVAL = [p[1][cell_id], p[1][lon], p[1][lat], val]
+                presValues.append(presVAL)
+            df = pd.DataFrame(presValues, columns=['cell_id', 'x', 'y', re.sub(".tif", "", rs)])
+        else:
+            for p in pos.iterrows():
+                x = int((p[1][lon] - x0)/w)
+                y = int((p[1][lat] - y0)/h)
+                if y < params[0] and x < params[1]:
+                    val = data[y,x]
+                else:
+                    val = -9999
+                presValues.append(val)
+            df[re.sub(".tif", "", rs)] = pd.Series(presValues)
+    del data, band
+    return df
+
+
+rasters = ["distance_to_city.tif"]
+
+new_grid = getValuesAtPoint(indir=path, rasterfileList=rasters, pos=grid, lon="lon", lat="lat", cell_id="cell_id")
+
+#temperature_grid = pd.concat([grid, temp.reset_index(drop=True).drop(["cell_id", "x", "y"], axis=1)], axis=1)
+
+temperature_grid.drop(["x", "y"], axis=1).to_csv(path+"/temperature_grid.csv", index=False)
+
+
+
+
+
+
+
+
+
+###########################################################################
+
 grid = grid_1km_data
 
 del grid_1km_data
